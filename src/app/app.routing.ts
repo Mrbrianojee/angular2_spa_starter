c33e26030df73b861d8ec7edb0c616cc09d45164
brianojee <experimenting@codeinstitute.net>
2017-04-24 15:59:33 +0100

    initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
+
+const appRoutes: Routes = [
+	{
+		path:'',
+		component: HomeComponent
+	},
+
+	{	
+		path:'about',
+		component: AboutComponent
+	},
+	{	
+		path:'contact',
+		component: HomeComponent
+	}
+
+]; 
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
